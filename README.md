3ef78d8cef68bc09dcc811b8aa66db2ca9767409
PavelShturm85 <pavel.shturm@gmail.com>
2017-12-02 23:59:44 +0400

    Скрипт меняющий размер изображения.

--- README.md
+++ README.md
@@ -1,7 +1,73 @@
 # Image Resizer
 
-[TODO. There will be project description]
+**This module resize image.**
 
+# How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+*Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.*
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+# Quickstart
+**Ways to use:**
+*- Have to use  module `image_resize.py` after `python3`, with `arguments`.*
+   - You must use `scale` without `width` or `height`!
+   - You must enter `width` or `height` or `scale` how argument!
+   - You may use `width` and `height`, but you may get message:
+   `***The proportions do not match the original image.***`
+
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python3 image_resize.py -h
+usage: image_resize.py [-h] [-w WIDTH] [-he HEIGHT] [-sc SCALE] [-out OUTPUT]
+                       image
+
+Module for resize image.
+
+positional arguments:
+  image                 Where get the image.
+
+optional arguments:
+  -h, --help            show this help message and exit
+
+  -w WIDTH, --width WIDTH           Input new width image.
+
+  -he HEIGHT, --height HEIGHT       Input new height image.
+
+  -sc SCALE, --scale SCALE Input    how scale image.
+
+  -out OUTPUT, --output OUTPUT      Where to put the image.
+
+```
+**RuntimeError example:**
+
+```bash
+$ python3 image_resize.py panda.jpg
+
+Traceback (most recent call last):
+  File "image_resize.py", line 71, in <module>
+    namespace.scale)
+  File "image_resize.py", line 42, in get_new_size
+    raise RuntimeError('Width or height or scale required!')
+RuntimeError: Width or height or scale required!
+```
+
+```bash
+python3 image_resize.py panda.jpg -w 900 -he 700 -sc 2
+Traceback (most recent call last):
+  File "image_resize.py", line 75, in <module>
+    namespace.scale)
+  File "image_resize.py", line 32, in get_new_size
+    raise RuntimeError('You must use scale without width or height!')
+RuntimeError: You must use scale without width or height!
+
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,89 @@
+import os
+import argparse
+from PIL import Image
 
 
-def resize_image(path_to_original, path_to_result):
-    pass
+def createParser():
+        parser = argparse.ArgumentParser(
+            description='Module for resize image.')
+        parser.add_argument(
+            'image', help='Where get the image.')
+        parser.add_argument(
+            '-w', '--width', type=int, help='Input new width image.')
+        parser.add_argument(
+            '-he', '--height', type=int, help='Input new height image.')
+        parser.add_argument(
+            '-sc', '--scale', type=float, help='Input how scale image.')
+        parser.add_argument(
+            '-out', '--output', help='Where to put the image.')
+        return parser
+
+
+def get_image(path_to_original):
+    return Image.open(path_to_original)
+
+
+def get_new_size(original_image,
+                 new_width,
+                 new_height,
+                 scale):
+
+    width_original, height_original = original_image.size
+    if new_width and new_height and scale:
+        raise RuntimeError('You must use scale without width or height!')
+    elif new_width and new_height:
+        if width_original / height_original != new_width / new_height:
+            print('***The proportions do not match the original image.***')
+        new_size = (new_width, new_height)
+    elif new_width:
+        height = int(new_width * height_original / width_original)
+        new_size = (new_width, height)
+    elif new_height:
+        width = int(new_height * width_original / height_original)
+        new_size = (width, new_height)
+    elif scale:
+        new_size = [round(scale * s) for s in original_image.size]
+    else:
+        raise RuntimeError('Width or height or scale required!')
+    return new_size
+
+
+def resize_image(original_image, new_size):
+    return original_image.resize(new_size, Image.ANTIALIAS)
+
+
+def chose_path_to_result(path_to_result, path_to_original, resized_image):
+    if path_to_result:
+        return path_to_result
+    else:
+        name_file, file_extension = os.path.splitext(path_to_original)
+
+        created_path = '{}__{}x{}{}'.format(
+            name_file,
+            *resized_image.size,
+            file_extension)
+        return created_path
+
+
+def save_image(resized_image, path_to_result):
+    resized_image.save(path_to_result)
 
 
 if __name__ == '__main__':
-    pass
+    parser = createParser()
+    namespace = parser.parse_args()
+
+    path_to_original = namespace.image
+    path_to_result = namespace.output
+    image = get_image(path_to_original)
+    new_size = get_new_size(image,
+                            namespace.width,
+                            namespace.height,
+                            namespace.scale)
+
+    resized_image = resize_image(image, new_size)
+
+    chosen_path_to_result = chose_path_to_result(
+        path_to_result, path_to_original, resized_image)
+
+    save_image(resized_image, chosen_path_to_result)
